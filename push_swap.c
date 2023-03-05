116c3baadb4710e52e8fb75796fcd62a13194c6d
adrgonza <105714518+adriiiiiiiii@users.noreply.github.com>
2023-03-06 00:57:52 +0100

    parseo done

--- push_swap.c
+++ push_swap.c
@@ -1,7 +1,60 @@
+#include "push_swap.h"
+
+int ft_get_number(int argc, char **argv, t_push *push) // there are leaks int this function
+{
+    char **splitted;
+    char *string;
+    int i;
+
+    string = malloc(1);
+    i = 0;
+    while(++i < argc) //put all arguments in a hole string
+    {
+        string = ft_strjoin(string, argv[i]);
+        string = ft_strjoin(string, " ");
+    }
+    splitted = ft_split(string, ' '); //split string
+    free(string);
+    i = 0;
+    while (splitted[i]) //get size of *int
+        i++;
+    push->nb_count = i;
+    push->nb = malloc(i); //allocate memory for it
+    i = -1;
+    while(splitted[++i]) //put every number in a int *
+    {
+        push->nb[i] = atoi(splitted[i]);
+        free(splitted[i]);
+    }
+    free(splitted);
+    return (1);
+}
+
 int main(int argc, char **argv)
 {
-    int nb;
+    t_push push;
+    int i;
+    int j;
 
-    if (argc <= 1)
-        return ();
+    if (argc <= 1) //check there is more than 1 argument
+        return (printf("\033[31mError:\nInsert Numbers...\n"));
+    j = 0;
+    while (++j < argc) //check there is only numbers and spaces
+    {
+        i = -1;
+        while (argv[j][++i])
+            if (argv[j][i] != ' '  && (argv[j][i] < '0' || argv[j][i] > '9'))
+                if((argv[j][i] != '-' || (argv[j][i + 1] < '0' || argv[j][i + 1] > '9')))
+                    return (printf("\033[31mError:\nOnly numbers allow...\n"));
+    }
+    ft_get_number(argc, argv, &push);
+    i = -1;
+    while (++i < push.nb_count)//check repeated numbers
+    {
+        j = i;
+        while (++j < push.nb_count)
+            if (push.nb[j] == push.nb[i])
+                return (printf("\033[31mError:\nRepeated numbers...\n"));
+    }
+
 }
---
+++ push_swap.h
@@ -0,0 +1,20 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft/libft.h"
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <string.h>
+# include <mlx.h>
+
+typedef struct push{
+    int *nb;
+	int nb_count;
+}				t_push;
+
+int main(int argc, char **argv);
+
+#endif
